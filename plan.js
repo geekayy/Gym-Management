980e3a9943c5715c1423230a68922f0806cb5232
codesahib <gursahib.kvm@gmail.com>
2023-04-05 00:38:32 +0530

    API function complete

--- plan.js
+++ plan.js
@@ -7,9 +7,9 @@ async function send_plan_query(req, res){
         res.statusMessage = "Cannot find details"
         res.status(401).end()
     } else {
-        const  first_name = details[first_name]
-        const  last_name = details[last_name]
-        const  phone = details[phone_number]
+        const  first_name = details.first_name
+        const  last_name = details.last_name
+        const  phone = details.phone_number
         const plan_query_details = {first_name, last_name, email, phone, plan_type}
         const plan_query_result = await User.send_plan_query(plan_query_details);
 
